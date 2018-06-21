474d21c27a51465f0f8cfc1b0e52f3b1f502efb4
Tanner Lancaster <tanner@flatironschool.com>
2018-06-21 04:15:39 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.grep["kiwi"]
+  if array.length == 2
+    array.join(" and ")
+  elsif array.length == 3
+    array[-1] = "and " + array.last
+    array.join(",  ")
+  end
 end
