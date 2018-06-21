5e5f182007f207a1e89fd2e7fc2f3730a792e9c3
Tanner Lancaster <tanner@flatironschool.com>
2018-06-21 04:22:44 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.first
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length == 3
+    array[-1] = "and " + array.last
+    array.join(", ")
+  elsif array.length > 3
+    array[-1] = "and " + array.last
+    array.join(", ")
+  end
 end
